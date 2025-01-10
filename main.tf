9dd8383bc388d925a6c60fa459cb35d40eaf0c3c
mitch-data <mdata4w@gmail.com>
2025-01-10 11:29:21 -0500

    comment out security group code, update module name

--- main.tf
+++ main.tf
@@ -32,22 +32,13 @@ resource "aws_instance" "blog" {
   }
 }
 
-# Define Security Group for AWS instance
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id      = data.aws_vpc.default.id
-}
-
 # Define module
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name    =  "blog_new"
+  name    =  "blog"
 
   vpc_id      = data.aws_vpc.default.id
-
 
   # https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/rules.tf
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
@@ -58,32 +49,41 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] # define networks
+# NTS: line 52 - 89 replaced by the module defined in line 43 - 50
+# Define Security Group for AWS instance
+# resource "aws_security_group" "blog" {
+#  name        = "blog"
+#  description = "Allow http and https in. Allow everything out"
+#
+#  vpc_id      = data.aws_vpc.default.id
+# }
 
-  security_group_id = aws_security_group.blog.id
-}
+#resource "aws_security_group_rule" "blog_http_in" {
+#  type        = "ingress"
+#  from_port   = 80
+#  to_port     = 80
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"] # define networks
+#
+#  security_group_id = aws_security_group.blog.id
+#}
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 433
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] # define networks
+#resource "aws_security_group_rule" "blog_https_in" {
+#  type        = "ingress"
+#  from_port   = 443
+#  to_port     = 433
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"] # define networks
+#
+#  security_group_id = aws_security_group.blog.id
+#}
 
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0  # allow any port
-  to_port     = 0 
-  protocol    = "-1" # allow all protocol
-  cidr_blocks = ["0.0.0.0/0"] # define networks
-
-  security_group_id = aws_security_group.blog.id
-}
+#resource "aws_security_group_rule" "blog_everything_out" {
+#  type        = "egress"
+#  from_port   = 0  # allow any port
+#  to_port     = 0 
+#  protocol    = "-1" # allow all protocol
+#  cidr_blocks = ["0.0.0.0/0"] # define networks
+#
+#  security_group_id = aws_security_group.blog.id
+#}
